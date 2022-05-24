d507fef314d158da342a483531525afc737f745f
PabloOsorix <3387@holbertonschool.com>
2022-05-23 20:38:54 -0500

    :heavy_plus_sign: add public method close()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,10 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """Public method that call reload()
+        for deserializing the JSON file to
+        objects
+        """
+        self.reload()
