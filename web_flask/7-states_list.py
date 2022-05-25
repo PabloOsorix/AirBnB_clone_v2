edae2a5c7a2fcdd34e25531351976899274a2330
PabloOsorix <3387@holbertonschool.com>
2022-05-24 23:38:14 -0500

    :pencil2: fix pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,12 @@
 """
 Script that starts a Flask web application
 it use storafe from  the storage engine.
-It have 4 routes 
-(1.route: /states_list) 
+It have 4 routes
+(1.route: /states_list)
 (2. route: H1 tag states)
 (3. route: UL taf list all states)
 (4. route: LI taf description of one state)
 """
-
 
 from models import storage
 from flask import Flask
@@ -24,9 +23,11 @@ def states_list():
     state_list = storage.all().copy()
     return render_template('7-states_list.html', html_list=state_list)
 
+
 @app.teardown_appcontext
 def close(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
